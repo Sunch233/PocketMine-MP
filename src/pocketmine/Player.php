e51d0d6fefe349caa68fcd1caade9dd96b48949a
Dylan K. Taylor <odigiman@gmail.com>
2016-09-06 11:23:49 +0100

    Inventory: Fix hotbar spaz

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1925,7 +1925,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 						}
 					}else{
 						if($packet->selectedSlot >= 0 and $packet->selectedSlot < 9){
-							$this->inventory->setHeldItemIndex($packet->selectedSlot);
+							$this->inventory->setHeldItemIndex($packet->selectedSlot, false);
 							$this->inventory->setHeldItemSlot($packet->slot);
 						}else{
 							$this->inventory->sendContents($this);
@@ -1936,20 +1936,18 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 					$this->inventory->sendContents($this);
 					break;
 				}elseif($this->isCreative()){
-					$this->inventory->setHeldItemIndex($packet->selectedSlot);
+					$this->inventory->setHeldItemIndex($packet->selectedSlot, false);
 					$this->inventory->setItem($packet->selectedSlot, $item);
 					$this->inventory->setHeldItemSlot($packet->selectedSlot);
 				}else{
 					if($packet->selectedSlot >= 0 and $packet->selectedSlot < $this->inventory->getHotbarSize()){
-						$this->inventory->setHeldItemIndex($packet->selectedSlot);
+						$this->inventory->setHeldItemIndex($packet->selectedSlot, false);
 						$this->inventory->setHeldItemSlot($slot);
 					}else{
 						$this->inventory->sendContents($this);
 						break;
 					}
 				}
-
-				$this->inventory->sendHeldItem($this->hasSpawned);
 
 				$this->setDataFlag(self::DATA_FLAGS, self::DATA_FLAG_ACTION, false);
 				break;
